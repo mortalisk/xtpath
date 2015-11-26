e643fa4a215d35af9f0e57cfd38f2e219156b81f
Morten Bendiksen <morten.bendiksen@gmail.com>
2015-11-26 12:36:24 +0100

    Correct lisence text. Add scopedmap.cpp to test target

--- test/test_scopedmap.cpp
+++ test/test_scopedmap.cpp
@@ -1,5 +1,5 @@
 
-//          Copyright Morten Bendiksen 2004 - 2006.
+//          Copyright Morten Bendiksen 2014 - 2016.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -8,11 +8,7 @@
 
 #include <unordered_map>
 
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE Hello
 #include <boost/test/unit_test.hpp>
-
 
 using namespace mediasequencer::plugin::util::xpath;
 
